0e818654eaae9cdd45f23a7e1e88d61b2044ac9c
danielxf007 <daniel.higuita007@gmail.com>
2022-03-22 13:09:07 -0500

    added components for auctions

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import './App.css';
 import { useEffect, useState } from "react";
 import Minter from './components/Minter'
-import Main_menu from './Main-menu';
 import SellPublisher from './components/Sell-Publish';
 import MarketPlace from './components/Sell-Board';
-import AuctionCreator  from './components/Auction-Publish';
 import AuctionBoard from './components/Auction-Board';
-import AuctionWithdrawer from './components/Withdraw-Auction-Offert';
-import {getJSON, getTokenUri} from './util/interact';
-var bigInt = require("big-integer");
+import AuctionCollector from './components/Auction-Collect';
+import AuctionCreator  from './components/Auction-Publish';
+import AuctionRenewer from './components/Auction-Renew';
+import AuctionBidWithdrawer from './components/Auction-Withdraw-Bid';
+import {
+  clearPinata
+} from "./util/pinata";
+
+
+
 function App() {
   const [component, setComponent] = useState("main_menu");
-  const components = {"main_menu": <Main_menu/>, "minter": <Minter/>,
-   "sell_publisher": <SellPublisher/>, "auction_creator": <AuctionCreator/>, "market_place": <MarketPlace/>,
-    "auction_board": <AuctionBoard/>, "auction_withdrawer": <AuctionWithdrawer />};
-
-  const f = async() => {
-    console.log("here");
-    let data = await getJSON("https://gateway.pinata.cloud/ipfs/QmQyUiaz5sTRdpYkJ8ZywSgLZTLyyMMYoGGTY58JfbCg73");
-    console.log(data)
-    data = await getTokenUri(0);
-    console.log(data);
-    console.log(bigInt(1000000000000000000000000000000000000).toString(16));
-  };
+  const components = {
+    "minter": <Minter/>, "sell_publisher": <SellPublisher/>,
+    "auction_creator": <AuctionCreator/>, "market_place": <MarketPlace/>,
+    "auction_board": <AuctionBoard/>, "auction_bid_withdrawer": <AuctionBidWithdrawer />,
+    "auction_collector": <AuctionCollector />, "auction_renewer": <AuctionRenewer />};
+  
 
   if(component === "main_menu"){
     return (
-      <div>
+      <div className="main-menu-options-container">
         <button onClick={() => setComponent("minter")}>Mint NFT</button>
         <button onClick={() => setComponent("sell_publisher")}>Sell</button>
         <button onClick={() => setComponent("auction_creator")}>Create Auction</button>
         <button onClick={() => setComponent("market_place")}>Market Place</button>
         <button onClick={() => setComponent("auction_board")}>Auction Board</button>
-        <button onClick={() => setComponent("auction_withdrawer")}>Withdraw Auction Bid</button>
-        <button onClick={f}>Fetch</button>
-        {components[components]}
+        <button onClick={() => setComponent("auction_bid_withdrawer")}>Withdraw Auction Bid</button>
+        <button onClick={() => setComponent("auction_collector")}>Collect Auction</button>
+        <button onClick={() => setComponent("auction_renwer")}>Renew Auction</button>
+        <button onClick={clearPinata}>clearPinata</button>
       </div>
     );    
   }else{
