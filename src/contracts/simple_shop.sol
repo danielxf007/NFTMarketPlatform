c8972ee48041a747285874a16d0606c7868783c6
danielxf007 <daniel.higuita007@gmail.com>
2022-03-22 13:15:07 -0500

    changed to receive nft

--- src/contracts/simple_shop.sol
+++ src/contracts/simple_shop.sol
@@ -1,62 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleNFTShop{
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "./minter.sol";
 
-    struct ShopData{
-        address payable _seller;
+contract NFTShop is IERC721Receiver{
+
+    string private ERR_NOT_ENOUGH = "You do not have enough ether"; 
+    string private ERR_ALREADY_SOLD = "This NFT was already sold";
+    struct Item{
+        address _seller;
         uint256 _price;
-        bool _active;
-    }
-    constructor(){
-        
+        bool _sold;
     }
 
-    mapping (uint256 => ShopData) private _shop_book;
-    uint256 [] private _token_ids;
+    MarketMinter private _token_storage;
+    mapping (uint256 => Item) private _book;
 
-    function add(address payable seller, uint256 price, uint256 token_id)
+    constructor(address token_storage)
         public
     {
-        _shop_book[token_id] = ShopData(seller, price, true);
+        _token_storage = MarketMinter(token_storage);
+    }
+
+    /**
+    * Always returns `IERC721Receiver.onERC721Received.selector`.
+    */
+    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    function publishSell(uint256 price, uint256 token_id)
+        external
+    {
+        _token_storage.safeTransferFrom(msg.sender, address(this), token_id);
+        _book[token_id] = Item(msg.sender, price, false);
+    }
+
+    function buy(uint256 token_id)
+        payable
+        external
+    {
+        Item memory item = _book[token_id];
+        require(msg.value == item._price, ERR_NOT_ENOUGH);
+        require(!item._sold, ERR_ALREADY_SOLD);
+        address payable seller = payable(item._seller);
+        seller.transfer(msg.value);
+       _token_storage.safeTransferFrom(address(this), msg.sender, token_id);
+        item._sold = true;
+        _book[token_id] = item;
     }
 
     function remove(uint256 token_id)
-        public
+        external
     {
-        _shop_book[token_id] = ShopData(payable (0), 0, false);
-    }
-
-    function sellerOf(uint256 token_id)
-        public
-        view
-        returns(address payable)
-    {
-        return _shop_book[token_id]._seller;
-    }
-
-    function priceOf(uint256 token_id)
-        public
-        view
-        returns(uint256)
-    {
-        return _shop_book[token_id]._price;
-    }
-
-    function isBeingSold(uint256 token_id)
-        public
-        view
-        returns(bool)
-    {
-        return _shop_book[token_id]._active;
-    }
-
-    function getSellData(uint256 token_id)
-        public
-        view
-        returns(ShopData memory)
-    {
-        return _shop_book[token_id];
+        _book[token_id] = Item(address(0), 0, false);
     }
 
 }
