a6eb77389d2ff32fe63f821e0416d9f9ef007fd0
danielxf007 <daniel.higuita007@gmail.com>
2022-04-04 09:29:18 -0500

    added alchemy webhook

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const path = require('path');
+const socketIO = require('socket.io');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, 'build');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-
 app.use(express.static("public"));
+app.use(express.json());
+
+app.post('/alchemyhook', (req, res) => {
+   notificationReceived(req); res.status(200).end() 
+});
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+// start the websocket server
+const io = socketIO(app);
+
+// listen for client connections/calls on the WebSocket server
+io.on('connection', (socket) => {
+   socket.emit('Greeting', 'Hello');
+});
+
+// notification received from Alchemy from the webhook. Let the clients know.
+function notificationReceived(req) {
+  io.emit('notification', JSON.stringify(req.body));
+}
 
 app.listen(port, () => {
    console.log('Server is up!');
