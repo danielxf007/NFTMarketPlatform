a820ba2d5a039db87ddc4a780a8008d034ae1b95
danielxf007 <daniel.higuita007@gmail.com>
2022-04-04 14:17:39 -0500

    imported React

--- src/components/sockets.js
+++ src/components/sockets.js
@@ -1,4 +1,5 @@
 import {io} from 'socket.io-client'
+import React from 'react';
 
 export const socket = io('https://salty-everglades-98832.herokuapp.com/');
 export const SocketContext = React.createContext();
