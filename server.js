f5bf66b560a7282bb46ec0152b161f93be5f686d
danielxf007 <daniel.higuita007@gmail.com>
2022-04-05 16:46:32 -0500

    changed socket

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.post('/tx-mined', async (req, res) => {
-   txMined(req);
+   const _res = await txMined(req);
    res.status(200).end(); 
 });
 
@@ -107,29 +107,27 @@ io.on('connection', (socket) => {
 });
 
 function minedMint(token_name){
-   socket.emit('mined-tx-mint', 'Your NFT ' + token_name + 'was successfully minted');
+   io.emit('mined-tx-mint', 'Your NFT ' + token_name + 'was successfully minted');
 }
 
 function rejectedMint(token_name){
-   socket.emit('rejected-tx-minted', 'Your NFT ' + token_name + 'could not be minted');
+   io.emit('rejected-tx-minted', 'Your NFT ' + token_name + 'could not be minted');
 }
 
 async function txMined(req) {
    const tx = JSON.stringify(req.body);
    const tx_hash = tx.hash;
-   io.emit('mined-tx-mint', tx);
-   /*
    const pinata_tx = await getPinList("status=pinned&metadata[name]="+tx_hash);
    let res;
    if(pinata_tx.length > 0){
       const pinata_tx_data = await getPinataJSON(pinata_tx[0].ipfs_pin_hash);
       switch(pinata_tx_data.type){
          case "mint":
+            minedMint(pinata_tx_data.token_name);
             res = await removePinFromIPFS(pinata_tx[0].ipfs_pin_hash);
             break;
       }
    }
-   */
 }
 
 async function txRejected(req) {
