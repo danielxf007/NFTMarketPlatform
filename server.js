53983b8bee2db5972ba2ac537acb74c358ec0877
danielxf007 <daniel.higuita007@gmail.com>
2022-04-05 16:31:34 -0500

    changed socket

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ function rejectedMint(token_name){
 
 async function txMined(req) {
    const tx = JSON.stringify(req.body);
-   io.emit.emit('mined-tx-mint', tx);
+   io.emit('mined-tx-mint', tx);
    /*
    const tx_hash = tx.fullTransaction.hash;
    const pinata_tx = await getPinList("status=pinned&metadata[name]="+tx_hash);
--- src/components/sockets.js
+++ src/components/sockets.js
@@ -2,4 +2,3 @@ import {io} from 'socket.io-client'
 import React from 'react';
 
 export const socket = io('https://nft-market-place-udea.herokuapp.com/');
-export const SocketContext = React.createContext();
