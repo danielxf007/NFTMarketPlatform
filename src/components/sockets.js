119e7a09e7c86eb1a86164c7fe24cdf99b253c40
danielxf007 <daniel.higuita007@gmail.com>
2022-04-05 12:18:28 -0500

    changed heroku link

--- src/components/sockets.js
+++ src/components/sockets.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
 import React from 'react';
 
-export const socket = io('https://salty-everglades-98832.herokuapp.com/');
+export const socket = io('https://nft-market-place-udea.herokuapp.com/');
 export const SocketContext = React.createContext();
