187da34fb5d712227a00a5c5ca0e2707a3861662
danielxf007 <daniel.higuita007@gmail.com>
2022-04-06 17:47:59 -0500

    added validations for selling

--- src/util/validations.js
+++ src/util/validations.js
@@ -9,3 +9,33 @@ export const usedName = async(token_name) => {
     const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
     return contract.methods.usedName(token_name).call();
 }
+
+export const tokenExists = async(token_name) => {
+    const contract_metadata = contracts_metadata.minter;
+    const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
+    return contract.methods.exists(token_name).call();
+}
+
+export const canTradeToken = async(token_name, user_address) => {
+    const contract_metadata = contracts_metadata.minter;
+    const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
+    return contract.methods.exists(token_name, user_address).call();   
+}
+
+export const sellPublished = async(token_name) => {
+    const contract_metadata = contracts_metadata.shop;
+    const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
+    return contract.methods.published(token_name).call();  
+}
+
+export const tokenSold = async(token_name) => {
+    const contract_metadata = contracts_metadata.shop;
+    const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
+    return contract.methods.sold(token_name).call();     
+}
+
+export const offersEnough = async(token_name, offer) => {
+    const contract_metadata = contracts_metadata.shop;
+    const contract = await new web3.eth.Contract(contract_metadata.abi, contract_metadata.address);
+    return contract.methods.isEnough(token_name, offer).call();      
+}
