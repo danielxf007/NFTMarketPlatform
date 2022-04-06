d1917292ebbfd5e642ae191e9c02ce4a8b93f628
danielxf007 <daniel.higuita007@gmail.com>
2022-04-05 20:27:48 -0500

    changed txMinted

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ async function txMined(req) {
    let res;
    if(pinata_tx.length > 0){
       const pinata_tx_data = await getPinataJSON(pinata_tx[0].ipfs_pin_hash);
-      io.emit('mined-tx-mint', pinata_tx_data.nft_name);
+      io.emit('mined-tx-mint', pinata_tx_data);
       switch(pinata_tx_data.type){
          case "mint":
             //minedMint(pinata_tx_data.nft_name);
