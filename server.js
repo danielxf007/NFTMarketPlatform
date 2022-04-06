8a7524fe7010f9c5171a9d2f0239ea98e1e52330
danielxf007 <daniel.higuita007@gmail.com>
2022-04-05 19:54:24 -0500

    changed socket

--- server.js
+++ server.js
@@ -126,7 +126,6 @@ function rejectedMint(token_name){
 async function txMined(req) {
    const tx = req.body;
    const pinata_tx = await getPinList("status=pinned&metadata[name]="+tx.hash);
-   io.emit('mined-tx-mint', pinata_tx.length);
    let res;
    if(pinata_tx.length > 0){
       const pinata_tx_data = await getPinataJSON(pinata_tx[0].ipfs_pin_hash);
