3b79f63b0752a55d751a5a4cd79d3b8cddd8d938
danielxf007 <daniel.higuita007@gmail.com>
2022-04-09 14:24:16 -0500

    added watch nft opt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect, useState } from "react";
 import Minter from './components/Minter'
+import OwnedBoard from './components/Owned-Board';
 import SellPublisher from './components/Sell-Publish';
 import MarketPlace from './components/Sell-Board';
 import AuctionBoard from './components/Auction-Board';
@@ -16,7 +17,7 @@ const pinata = require("./util/pinata.js");
 function App() {
   const [component, setComponent] = useState("main_menu");
   const components = {
-    "minter": <Minter socket={socket}/>, "sell_publisher": <SellPublisher socket={socket}/>,
+    "minter": <Minter socket={socket}/>, "owned_board": <OwnedBoard/>, "sell_publisher": <SellPublisher socket={socket}/>,
     "auction_creator": <AuctionCreator/>, "market_place": <MarketPlace socket={socket}/>,
     "auction_board": <AuctionBoard/>, "auction_bid_withdrawer": <AuctionBidWithdrawer />,
     "auction_collector": <AuctionCollector />, "auction_renewer": <AuctionRenewer />};
@@ -54,6 +55,7 @@ function App() {
     return (
       <div className="main-menu-options-container">
         <button onClick={() => setComponent("minter")}>Mint NFT</button>
+        <button onClick={() => setComponent("owned_board")}>Watch your NFTs</button>
         <button onClick={() => setComponent("sell_publisher")}>Sell</button>
         <button onClick={() => setComponent("auction_creator")}>Create Auction</button>
         <button onClick={() => setComponent("market_place")}>Market Place</button>
