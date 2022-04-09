d1e47d87d697543df8febec8397656b4ba7b725b
danielxf007 <daniel.higuita007@gmail.com>
2022-04-09 14:23:37 -0500

    added convinience

---
+++ src/components/Owned-Board.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import './Board.css';
+import { getTokens} from "../util/interact";
+import ReactPaginate from 'react-paginate';
+
+
+const BoardCell = (props) => {
+
+    return (
+        <div className="nft-item">
+          <div className="nft-name">
+            {props.name}
+          </div>
+          <img className="nft-image" src={props.image_url}/>
+        </div>
+    );
+}
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }) {
+  return (
+    <>
+      {
+        currentItems && currentItems.map((item, index) =>{
+          return <BoardCell
+                  key={String(index)}
+                  name={item.name}
+                  image_url={item.image_url}
+                  />
+        })
+      }
+    </>
+  );
+}
+
+function PaginatedItems({ itemsPerPage, socket }) {
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    const getNFTs= async() => {
+      const items = await getTokens();
+      console.log(items);
+      setCurrentItems([].slice(itemOffset, endOffset));
+      setPageCount(Math.ceil([].length / itemsPerPage));
+    }
+    getNFTs();
+  }, [itemOffset, itemsPerPage]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+    <div className="nft-item-container">
+    <Items currentItems={currentItems} />
+    </div>
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
+
+const OwnedBoard = (props) => {
+    return (
+        <div>
+              <PaginatedItems itemsPerPage={10}/>
+        </div>
+    );
+}
+
+export default OwnedBoard;
