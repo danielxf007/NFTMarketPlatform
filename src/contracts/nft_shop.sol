3ebb3ade9d05aa4298b96b757d39cdc8dd2e186c
danielxf007 <daniel.higuita007@gmail.com>
2022-04-11 18:25:00 -0500

    updated contracts

--- src/contracts/nft_shop.sol
+++ src/contracts/nft_shop.sol
@@ -7,52 +7,136 @@ import "./nft_storage.sol";
 
 contract NFTShop is IERC721Receiver{
 
-    string private ERR_NOT_ENOUGH = "You do not have enough ether"; 
-    string private ERR_ALREADY_SOLD = "This NFT was already sold";
+    string private ERR_NO_TOKEN = "This token does not exist";
+    string private ERR_CANNOT_TRADE = "You cannot trade this token";
+    string private ERR_ALREADY_PUBLISHED = "This token is already been sold";
+    string private ERR_NOT_PUBLISHED = "This token is not been sold";
+    string private ERR_NOT_ENOUGH = "You do not have enough ether";
+    string private ERR_ALREADY_SOLD = "This token was already sold";
 
     struct Item{
-        address _seller;
+        string _name;
+        string _image_url;
         uint256 _price;
-        bool _sold;
+        address _seller;
     }
 
     TokenStorage private _token_storage;
-    mapping (uint256 => Item) private _book;
+    mapping (string => bool) private _sold_history;
+    mapping (string => bool) private _publish_history;
+    mapping (string => uint256) private _arr_indexing;
+    Item[] private _inventory;
 
     constructor(address token_storage)
         public
     {
         _token_storage = TokenStorage(token_storage);
+        _inventory.push(Item("", "", 0,  address(0)));
+        _arr_indexing[""] = 0;
     }
 
     /**
+    *@dev this interface allows this contract to get tokens transfered to it
     * Always returns `IERC721Receiver.onERC721Received.selector`.
     */
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
         return this.onERC721Received.selector;
     }
 
+    /*
+    *@dev checks if the token has already been published
+    */    
+    function published(string memory token_name)
+        public
+        view
+        returns (bool)
+    {
+        return _publish_history[token_name];
+    }
+
+    /*
+    *@dev allows a user to pulish an offert for their tokens
+    */ 
     function publishSell(string memory token_name, uint256 price)
         external
     {
+        require(_token_storage.exists(token_name), ERR_NO_TOKEN);
+        require(_token_storage.canTrade(token_name, msg.sender), ERR_CANNOT_TRADE);
+        require(!published(token_name), ERR_ALREADY_PUBLISHED);
         uint256 token_id = _token_storage.getTokenId(token_name);
         _token_storage.safeTransferFrom(msg.sender, address(this), token_id);
-        _book[token_id] = Item(msg.sender, price, false);
+        _publish_history[token_name] = true;
+        _sold_history[token_name] = false;
+        _inventory.push(Item(token_name, _token_storage.tokenURI(token_id), price, msg.sender));
+        _arr_indexing[token_name] = _inventory.length-1;
     }
 
+    function getPublishedSells()
+        public
+        view
+        returns (Item[] memory)
+    {
+        return _inventory;
+    }
+
+    /*
+    *@dev allows a user to pulish an offert for their tokens
+    */ 
+    function sold(string memory token_name)
+        public
+        view
+        returns (bool)
+    {
+        return _sold_history[token_name];
+    }
+
+    /*
+    *@dev checks if the offer made matches the token's price
+    */    
+    function isEnough(string memory token_name, uint256 offer)
+        public
+        view
+        returns (bool)
+    {
+        return _inventory[_arr_indexing[token_name]]._price == offer;
+    }
+        
+    /*
+    *@dev removes an item from the inventory
+    */        
+    function removeItem(string memory token_name)
+        public
+    {
+        uint256 index = _arr_indexing[token_name];
+        uint256 last_index = _inventory.length-1;
+        if(index != last_index){
+            _inventory[index] = _inventory[last_index];
+            _arr_indexing[_inventory[index]._name] = index;
+        }
+        _inventory.pop();
+        _arr_indexing[token_name] = 0;
+    }
+    /*
+    *@dev allows a user to buy a published token
+    *after the token is transfered the rights over the token given to this contract,
+    * get taken away
+    */
     function buy(string memory token_name)
         payable
         external
     {
+        require(_token_storage.exists(token_name), ERR_NO_TOKEN);
+        require(published(token_name), ERR_NOT_PUBLISHED);
+        require(!sold(token_name), ERR_ALREADY_SOLD);
+        require(isEnough(token_name, msg.value), ERR_NOT_ENOUGH);
         uint256 token_id = _token_storage.getTokenId(token_name);
-        Item memory item = _book[token_id];
-        require(msg.value == item._price, ERR_NOT_ENOUGH);
-        require(!item._sold, ERR_ALREADY_SOLD);
+        Item memory item = _inventory[_arr_indexing[token_name]];
         address payable seller = payable(item._seller);
         seller.transfer(msg.value);
         _token_storage.safeTransferFrom(address(this), msg.sender, token_id);
-        item._sold = true;
-        _book[token_id] = item;
+        removeItem(token_name);
+        _publish_history[token_name] = false;
+        _sold_history[token_name] = true;
     }
 
 }
--- src/contracts/nft_storage.sol
+++ src/contracts/nft_storage.sol
@@ -7,10 +7,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract TokenStorage is ERC721URIStorage{
 
     string private ERR_ALREADY_STORED = "There is already an NFT using this name";
-
     using Counters for Counters.Counter;
     Counters.Counter private _token_ids;
-    mapping(string => uint256) private _storage;
+    struct TokenData{
+        string _name;
+        string _image_url;
+    }
+    mapping(string => uint256) private _storage; // (name, token_id) pairs
+    mapping(address => mapping(uint256 => uint256)) private _arr_indexing; // ((wallet_addr, token_id), arr_index)
+    mapping(address => TokenData[]) private _tokens; // (wallet_addr, [token_0, ..., token_i])
     
     /*
     *@dev initializes the TokenStorage
@@ -23,15 +28,71 @@ contract TokenStorage is ERC721URIStorage{
     *@dev creates a token and gives it to the recipient,
     *if the name has already been used the function fails
     */
-    function mintNFT(string memory token_name, address recipient, string memory token_uri)
+    function mintNFT(string memory token_name, string memory image_url, address recipient)
         external
     {
         require(!usedName(token_name), ERR_ALREADY_STORED);
         uint256 item_id = _token_ids.current();
-        _mint(recipient, item_id);
-        _setTokenURI(item_id, token_uri);
+        uint256 last_index;
+        TokenData memory token_data;
         _storage[token_name] = item_id;
+        token_data = TokenData(token_name, image_url);
+        _tokens[recipient].push(token_data);
+        last_index = _tokens[recipient].length-1;
+        _arr_indexing[recipient][item_id] = last_index;
+        _mint(recipient, item_id);
+        _setTokenURI(item_id, image_url);
         _token_ids.increment();
+    }
+
+    /*
+    *@dev removes a token from the address
+    */ 
+    function removeToken(address from, uint256 token_id)
+        private
+        returns (TokenData memory)
+    {
+        uint256 index = _arr_indexing[from][token_id];
+        uint256 last_index = _tokens[from].length-1;
+        TokenData memory token = _tokens[from][index];
+        TokenData memory last_token;
+        if(index != last_index){
+            last_token = _tokens[from][last_index];
+            _tokens[from][index] = last_token;
+            _arr_indexing[from][getTokenId(last_token._name)] = index;
+        }
+        _tokens[from].pop();
+        _arr_indexing[from][token_id] = 0;
+        return token;
+    }
+
+    /*
+    *@dev changes tokes who belong to from and gives it to to
+    *when address(0) means that the token was minted
+    */    
+    function _afterTokenTransfer(address from, address to,uint256 tokenId)
+        internal
+        virtual
+        override 
+    {
+        super._afterTokenTransfer(from, to, tokenId);
+        if(from != address(0)){
+            TokenData memory token = removeToken(from, tokenId);
+            _tokens[to].push(token);
+            _arr_indexing[to][tokenId] = _tokens[to].length-1;
+        }
+    }
+
+
+    /*
+    *@dev retrives the tokens that belong to the wallet
+    */
+    function getTokens(address wallet)
+        public
+        view
+        returns(TokenData[] memory)
+    {
+        return _tokens[wallet];
     }
 
     /*
